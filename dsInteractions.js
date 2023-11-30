559459847c72da8fa701accbfbaca76cd6cc14bf
Matthew Anthony <misterbaboo@gmail.com>
2023-11-30 12:30:47 -0500

    Add alert to user if Google service is unavailable

--- dsInteractions.js
+++ dsInteractions.js
@@ -21,21 +21,35 @@ module.exports = (client) => {
 				return;
 			}
 		} catch (error) {
-			console.error(error);
+			if (process.env.BOT_NAME == 'test') {
+				console.error(error);
+			} else {
+				console.error(error);
 
-			let errTime = moment().format('MMMM Do YYYY, h:mm:ss a');;
-			let fileParts = __filename.split(/[\\/]/);
-			let fileName = fileParts[fileParts.length - 1];
+				let errTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+				let fileParts = __filename.split(/[\\/]/);
+				let fileName = fileParts[fileParts.length - 1];
 
-			console.log(`Error occured at ${errTime} at file ${fileName}!`);
+				console.log(`An error occured at ${errTime} at file ${fileName}!`);
 
-			let errorEmbed = [new EmbedBuilder()
-				.setTitle(`An error occured on the ${process.env.BOT_NAME} bot file ${fileName}!`)
-				.setDescription(`\`\`\`${error.toString().slice(0, 2000)}\`\`\``)
-				.setColor('B80600')
-				.setFooter({ text: `${errTime}` })];
+				let errString = error.toString();
 
-			await interaction.client.channels.cache.get(process.env.ERROR_LOG_CHANNEL_ID).send({ embeds: errorEmbed });
+				if (errString === 'Error: The service is currently unavailable.') {
+					try {
+						await interaction.editReply({ content: `⚠ A service provider we use has had a temporary outage. Please try to submit your request again.`, ephemeral: true });
+					} catch {
+						await interaction.reply({ content: `⚠ A service provider we use has had a temporary outage. Please try to submit your request again.`, ephemeral: true });
+					}
+				}
+
+				let errorEmbed = [new EmbedBuilder()
+					.setTitle(`An error occured on the ${process.env.BOT_NAME} bot file ${fileName}!`)
+					.setDescription(`\`\`\`${errString}\`\`\``)
+					.setColor('B80600')
+					.setFooter({ text: `${errTime}` })];
+
+				await interaction.client.channels.cache.get(process.env.ERROR_LOG_CHANNEL_ID).send({ embeds: errorEmbed });
+			}
 		}
 	});
 };
